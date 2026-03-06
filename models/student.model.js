1570b6b42725957579195b80f5c8c43225eb0786
Tanish Bhutani <65407991+tanish14bot@users.noreply.github.com>
2026-03-06 16:56:09 +0000

    Create student model with schema and validation

---
+++ models/student.model.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const studentSchema = new mongoose.Schema({
+      name: {
+                type: String,
+                required: true,
+                trim: true,
+                minlength: 2
+      },
+      roll: {
+                type: Number,
+                required: true,
+                unique: true,
+                min: 1
+      }
+}, {
+      timestamps: true
+});
+
+const Student = mongoose.model('Student', studentSchema);
+
+export default Student;
