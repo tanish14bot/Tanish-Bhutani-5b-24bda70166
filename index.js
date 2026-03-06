cd0ba9bcecb193c8d08e82ee0ea044f874991268
Tanish Bhutani <65407991+tanish14bot@users.noreply.github.com>
2026-03-06 16:54:04 +0000

    Initialize Express server with routes and middleware

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db.js';
+import studentRoutes from './routes/student.routes.js';
+import viewRoutes from './routes/student.view.routes.js';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/students', studentRoutes);
+app.use('/', viewRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+});
