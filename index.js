c417515a4915a28e01d1845b686056ce9952fd21
Tanish Bhutani <65407991+tanish14bot@users.noreply.github.com>
2026-03-06 17:26:34 +0000

    Fix console log indentation and export statement

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use('/', viewRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+        console.log(`Server is running on port ${PORT}`);
 });
-export default app;
+
 export default app;
